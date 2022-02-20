13b49ea5439c5f122f2bac3325bc88bfd54f52f6
Stijn Hoop <stijn@sandcat.nl>
2022-02-20 16:53:49 +0100

    Add boot options
    Note that I did not manage to get AMT to boot
    into  BIOS setup despite all docs.

--- getinfo.py
+++ getinfo.py
@@ -1,5 +1,8 @@
+from itertools import chain
 from os import environ
 import subprocess
+
+# import time
 from typing import Dict, Any, cast
 
 from lxml import etree
@@ -18,6 +21,7 @@ class WSManClient:
 
     XSD = "http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd"
     ADR = "http://schemas.xmlsoap.org/ws/2004/08/addressing"
+    SOAPENV = "http://www.w3.org/2003/05/soap-envelope"
 
     BOOTCAPABILITIES = [
         # https://software.intel.com/sites/manageability/AMT_Implementation_and_Reference_Guide/default.htm?turl=HTMLDocuments%2FWS-Management_Class_Reference%2FAMT_BootCapabilities.htm
@@ -49,6 +53,46 @@ class WSManClient:
         "AMTSecureBootControl",
         "UEFIWiFiCoExistenceAndProfileShare",
     ]
+
+    BOOTSETTINGDATA = {
+        "BIOSPause": "false",
+        "BIOSSetup": "false",
+        "BootMediaIndex": "0",
+        "ConfigurationDataReset": "false",
+        "EnforceSecureBoot": "false",
+        "FirmwareVerbosity": "0",
+        "ForcedProgressEvents": "false",
+        "IDERBootDevice": "0",
+        "LockKeyboard": "false",
+        "LockPowerButton": "false",
+        "LockResetButton": "false",
+        "LockSleepButton": "false",
+        "ReflashBIOS": "false",
+        "SecureErase": "false",
+        "UseIDER": "false",
+        "UseSOL": "false",
+        "UseSafeMode": "false",
+        "UserPasswordBypass": "false",
+    }
+
+    BOOTCHANGERESULTS = {
+        # https://software.intel.com/sites/manageability/AMT_Implementation_and_Reference_Guide/default.htm?turl=HTMLDocuments%2FWS-Management_Class_Reference%2FCIM_BootConfigSetting.htm%23ChangeBootOrder
+        "0": "Completed with No Error",
+        "1": "Not Supported",
+        "2": "Unknown/Unspecified Error",
+        "3": "Busy",
+        "4": "Invalid Reference",
+        "5": "Invalid Parameter",
+        "6": "Access Denied",
+        # 7..32767 -> Method Reserved
+        # 32768..65535 -> Vendor Specified
+    }
+
+    BOOTCONFIGROLE = {
+        # https://software.intel.com/sites/manageability/AMT_Implementation_and_Reference_Guide/default.htm?turl=WordDocuments%2Fsetordisablebootconfigurationsettingsforthenextboot.htm
+        "1": "IsNextSingleUse",
+        "32768": "IsNotNext",
+    }
 
     POWERSTATES = {
         # https://software.intel.com/sites/manageability/AMT_Implementation_and_Reference_Guide/default.htm?turl=WordDocuments%2Fchangesystempowerstate.htm
@@ -157,7 +201,6 @@ class WSManClient:
     def get_boot_capabilities(self) -> list[str]:
         xmlns = f"{self.AMT}/AMT_BootCapabilities"
         raw_xml = self.wsman_retrieval("get", xmlns)
-        print(raw_xml)
         tree = etree.fromstring(bytes(raw_xml, encoding="utf-8"))
         capabilities: list[str] = []
         for cap in self.BOOTCAPABILITIES:
@@ -168,7 +211,177 @@ class WSManClient:
                 raise ValueError(f"Invalid capability value {cap}")
             if cap_element.text == "true":
                 capabilities.append(cap)
+            elif cap_element.text != "false":
+                capabilities.append(cap)
         return capabilities
+
+    def _parse_bootparams(self, xmlns: str, raw_xml: str) -> dict[str, str]:
+        tree = etree.fromstring(bytes(raw_xml, encoding="utf-8"))
+        fault = tree.find(f".//{{{self.SOAPENV}}}Fault")
+        if fault is not None:
+            text = tree.find(f".//{{{self.SOAPENV}}}Reason//{{{self.SOAPENV}}}Text")
+            if text is None or text.text is None:
+                raise ValueError("Unknown error")
+            raise ValueError(text.text)
+
+        params: dict[str, str] = {}
+        for par in self.BOOTSETTINGDATA.keys():
+            par_element = tree.find(f".//{{{xmlns}}}{par}")
+            if par_element is None:
+                continue
+            if par_element.text is None:
+                raise ValueError(f"Invalid boot setting data for {par}")
+            params[par] = par_element.text
+        instance_id_element = tree.find(f".//{{{xmlns}}}InstanceID")
+        if instance_id_element is None or instance_id_element.text is None:
+            raise ValueError("Missing InstanceID")
+        params["InstanceID"] = instance_id_element.text
+        elementname_element = tree.find(f".//{{{xmlns}}}ElementName")
+        if elementname_element is None or elementname_element.text is None:
+            raise ValueError("Missing ElementName")
+        params["ElementName"] = elementname_element.text
+        return params
+
+    def get_bootparams(self) -> dict[str, str]:
+        selector = "InstanceID=Intel(r)%20AMT:BootSettingData%200"
+        xmlns = f"{self.AMT}/AMT_BootSettingData"
+        raw_xml = self.wsman_retrieval("get", f"{xmlns}?{selector}")
+        return self._parse_bootparams(xmlns, raw_xml)
+
+    def clear_bootparams(self) -> dict[str, str]:
+        return self.set_bootparams({})
+
+    def set_bootparams(self, params: dict[str, str]) -> dict[str, str]:
+        # See step 3 of
+        #   https://software.intel.com/sites/manageability/AMT_Implementation_and_Reference_Guide/default.htm?turl=WordDocuments%2Fsetsolstorageredirectionandotherbootoptions.htm
+        selector = "InstanceID=Intel(r)%20AMT:BootSettingData%200"
+        xmlns = f"{self.AMT}/AMT_BootSettingData"
+        wsman_args = chain.from_iterable([["-k", f"{key}={val}"] for key, val in (self.BOOTSETTINGDATA | params).items()])
+        raw_xml = self.wsman_retrieval(
+            "put",
+            f"{xmlns}?{selector}",
+            *wsman_args,
+        )
+        return self._parse_bootparams(xmlns, raw_xml)
+
+    def _check_bootorder_result(self, xmlns: str, raw_xml: str) -> str:
+        tree = etree.fromstring(bytes(raw_xml, encoding="utf-8"))
+        returnvalue = tree.find(f".//{{{xmlns}}}ReturnValue")
+        if (
+            returnvalue is None
+            or returnvalue.text is None
+            or self.BOOTCHANGERESULTS.get(returnvalue.text) is None
+        ):
+            raise ValueError("Could not determine boot change result")
+        return self.BOOTCHANGERESULTS[returnvalue.text]
+
+    def clear_bootorder(self) -> str:
+        selector = "InstanceID=Intel(r)%20AMT:%20Boot%20Configuration%200"
+        xmlns = f"{self.CIM}/CIM_BootConfigSetting"
+        raw_xml = self.wsman_retrieval(
+            "invoke", "-a", "ChangeBootOrder", "-d", "6", f"{xmlns}?{selector}"
+        )
+        return self._check_bootorder_result(xmlns, raw_xml)
+
+    def set_bootorder_pxe(self) -> str:
+        selector = "InstanceID=Intel(r)%20AMT:%20Boot%20Configuration%200"
+        xmlns = f"{self.CIM}/CIM_BootConfigSetting"
+        nsmap: Dict[str | None, str] = {"p": xmlns, "a": self.ADR, "x": self.XSD}
+
+        P = ElementMaker(namespace=xmlns, nsmap=nsmap)
+        REQUEST = P.ChangeBootOrder_INPUT
+        SOURCE = P.Source
+        A = ElementMaker(namespace=self.ADR, nsmap=nsmap)
+        ADDRESS = A.Address
+        REFERENCEPARAMETERS = A.ReferenceParameters
+        X = ElementMaker(namespace=self.XSD, nsmap=nsmap)
+        RESOURCEURI = X.ResourceURI
+        SELECTORSET = X.SelectorSet
+        SELECTOR = X.Selector
+
+        request: Any = REQUEST(
+            SOURCE(
+                ADDRESS(f"http://{self.host}:{self.port}/wsman"),
+                REFERENCEPARAMETERS(
+                    RESOURCEURI(f"{self.CIM}/CIM_BootSourceSetting"),
+                    SELECTORSET(
+                        SELECTOR("Intel(r) AMT: Force PXE Boot", Name="InstanceID")
+                    ),
+                ),
+            ),
+        )
+        input_xml = etree.tostring(request, pretty_print=True).decode("utf-8")
+        raw_xml = self.wsman_input(
+            input_xml,
+            "invoke",
+            "-a",
+            "ChangeBootOrder",
+            f"{xmlns}?{selector}",
+        )
+        return self._check_bootorder_result(xmlns, raw_xml)
+
+    def _bootconfig_to_internal_bootconfig(self, bootconfig: str) -> str:
+        internal_bootconfig: str | None = None
+        if bootconfig not in self.BOOTCONFIGROLE.keys():
+            for key, val in self.BOOTCONFIGROLE.items():
+                if val == bootconfig:
+                    internal_bootconfig = key
+                    break
+        else:
+            internal_bootconfig = bootconfig
+        if internal_bootconfig is None:
+            raise ValueError(f"Invalid boot config role {bootconfig} specified")
+        return internal_bootconfig
+
+    def set_bootconfig(self, cfg: str):
+        internal_cfg = self._bootconfig_to_internal_bootconfig(cfg)
+        selector = "Name=Intel(r)%20AMT%20Boot%20Service"
+        xmlns = f"{self.CIM}/CIM_BootService"
+        nsmap: Dict[str | None, str] = {"p": xmlns, "a": self.ADR, "x": self.XSD}
+
+        P = ElementMaker(namespace=xmlns, nsmap=nsmap)
+        REQUEST = P.SetBootConfigRole_INPUT
+        BOOTCONFIGSETTING = P.BootConfigSetting
+        ROLE = P.Role
+        A = ElementMaker(namespace=self.ADR, nsmap=nsmap)
+        ADDRESS = A.Address
+        REFERENCEPARAMETERS = A.ReferenceParameters
+        X = ElementMaker(namespace=self.XSD, nsmap=nsmap)
+        RESOURCEURI = X.ResourceURI
+        SELECTORSET = X.SelectorSet
+        SELECTOR = X.Selector
+
+        request: Any = REQUEST(
+            BOOTCONFIGSETTING(
+                ADDRESS(f"http://{self.host}:{self.port}/wsman"),
+                REFERENCEPARAMETERS(
+                    RESOURCEURI(f"{self.CIM}/CIM_BootConfigSetting"),
+                    SELECTORSET(
+                        SELECTOR(
+                            "Intel(r) AMT: Boot Configuration 0", Name="InstanceID"
+                        )
+                    ),
+                ),
+            ),
+            ROLE(internal_cfg),
+        )
+        input_xml = etree.tostring(request, pretty_print=True).decode("utf-8")
+        raw_xml = self.wsman_input(
+            input_xml,
+            "invoke",
+            "-a",
+            "SetBootConfigRole",
+            f"{xmlns}?{selector}",
+        )
+        tree = etree.fromstring(bytes(raw_xml, encoding="utf-8"))
+        returnvalue = tree.find(f".//{{{xmlns}}}ReturnValue")
+        if (
+            returnvalue is None
+            or returnvalue.text is None
+            or self.BOOTCHANGERESULTS.get(returnvalue.text) is None
+        ):
+            raise ValueError("Could not determine boot change result")
+        return self.BOOTCHANGERESULTS[returnvalue.text]
 
     def get_power_change_capabilities(self) -> dict[str, list[str]]:
         xmlns = f"{self.CIM}/CIM_PowerManagementCapabilities"
@@ -201,7 +414,7 @@ class WSManClient:
 
         return capabilities
 
-    def powerstate_to_internal_state(self, state: str) -> str:
+    def _powerstate_to_internal_state(self, state: str) -> str:
         internal_state: str | None = None
         if state not in self.POWERSTATES.keys():
             for key, val in self.POWERSTATES.items():
@@ -244,7 +457,7 @@ class WSManClient:
         return powerstate
 
     def set_power_state(self, state: str) -> str:
-        internal_state = self.powerstate_to_internal_state(state)
+        internal_state = self._powerstate_to_internal_state(state)
         selector = "Name=Intel(r)%20AMT%20Power%20Management%20Service"
         xmlns = f"{self.CIM}/CIM_PowerManagementService"
         nsmap: Dict[str | None, str] = {"p": xmlns, "a": self.ADR, "x": self.XSD}
@@ -298,12 +511,24 @@ if __name__ == "__main__":
     if password is None:
         raise ValueError("Need AMT password in environ AMT_PASSWORD")
     client = WSManClient(host, port, user, password)
+
     # print(client.get_power_change_capabilities())
     # print(client.get_power_state())
+    # print(client.set_power_state("Power Off - Soft"))
+    # time.sleep(5)
+    # print(client.get_power_state())
+    # print(client.set_power_state("Power Off - Soft Graceful"))
+    # time.sleep(5)
+    # print(client.get_power_state())
+
+    # Follow the steps here in order, or AMT will not do the right thing:
+    # https://software.intel.com/sites/manageability/AMT_Implementation_and_Reference_Guide/default.htm?turl=WordDocuments%2Fsetsolstorageredirectionandotherbootoptions.htm
+    # print(client.get_boot_capabilities())
+    # print(client.clear_bootparams())
+    # print(client.clear_bootorder())
+    # print(client.set_bootorder_pxe())
+    # print(client.set_bootconfig("IsNextSingleUse"))
     # print(client.set_power_state("On"))
-    # sleep(5)
+    # time.sleep(5)
     # print(client.get_power_state())
     # print(client.set_power_state("Power Off - Soft"))
-    # sleep(5)
-    # print(client.get_power_state())
-    print(client.get_boot_capabilities())
